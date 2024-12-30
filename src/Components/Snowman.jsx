30cd17bd97537d08013350a194406be8a053a823
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-12-30 20:52:58 +0530

    feat: add Snowman component

---
+++ src/Components/Snowman.jsx
@@ -0,0 +1,10 @@
+import "./Snowman.css";
+function Snowman() {
+  return (
+    <div className="snowman-container">
+      <div className="snowman"></div>
+    </div>
+  );
+}
+
+export default Snowman;
