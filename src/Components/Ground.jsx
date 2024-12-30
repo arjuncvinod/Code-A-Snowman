20c65c1427b14b09eae8b4463f504b80fe660a77
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-12-30 20:51:58 +0530

    feat: add Ground component with snowdrifts

---
+++ src/Components/Ground.jsx
@@ -0,0 +1,9 @@
+const Ground = () => (
+  <div className="ground">
+    <div className="ground__snowdrift"></div>
+    <div className="ground__snowdrift"></div>
+    <div className="ground__snowdrift"></div>
+  </div>
+);
+
+export default Ground;
