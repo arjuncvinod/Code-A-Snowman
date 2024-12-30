95604adc3a820402d3545b9f477186519573a081
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-12-30 20:53:26 +0530

    feat: add Snowflake component with animated snowflakes effect

---
+++ src/Components/Snowflake.jsx
@@ -0,0 +1,25 @@
+import "./Snowflake.css";
+
+const Snowflake = () => {
+  return (
+    <div className="snowflakes">
+      {Array.from({ length: 100 }).map((_, index) => {
+        const randomDelay = `${Math.random() * 5}s`;
+        const randomDuration = `${6 + Math.random() * 6}s`;
+        const randomOpacity = Math.random() * 0.7 + 0.3;
+        return (
+          <i
+            key={index}
+            style={{
+              animationDelay: randomDelay,
+              animationDuration: randomDuration,
+              opacity: randomOpacity,
+            }}
+          ></i>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Snowflake;
