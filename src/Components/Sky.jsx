d2056d273c96e9da01bfd1588ac15c466e155e85
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-12-30 20:52:09 +0530

    feat: add Sky component with stars and crescent

---
+++ src/Components/Sky.jsx
@@ -0,0 +1,13 @@
+const Sky = () => (
+  <div className="sky">
+    <div className="sky__stars"></div>
+    <div className="sky__stars" style={{ left: "10%" }}></div>
+    <div className="sky__stars" style={{ left: "30%" }}></div>
+    <div className="sky__stars" style={{ left: "60%" }}></div>
+    <div className="sky__stars" style={{ top: "80%" }}></div>
+    <div className="sky__stars" style={{ left: "20%", top: "70%" }}></div>
+    <div className="sky__crescent"></div>
+  </div>
+);
+
+export default Sky;
